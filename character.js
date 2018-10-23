e3041062f0a54766e524fd0420aafa857610c727
snegler <trondss@protonmail.ch>
2018-10-23 14:56:34 +0200

    Fiksa funksjon for hitpoint

--- character.js
+++ character.js
@@ -1,8 +1,10 @@
+//Sjølve spelaren
+
 const player = {
   name: null,
   age: null,
   gender: null,
-  hp: hitPoints(),
+  hitPoints: null,
 
   stats: {
     strength: null,
@@ -27,24 +29,19 @@ const player = {
     largeWeapon: null,
     smallWeapon1: null,
     smallWeapon2: null,
+    smallItem1: null,
+    smallItem2: null,
+    smallItem3: null,
+    smallItem4: null,
+    smallItem5: null,
+    largeItem1: null,
+    largeItem2: null
   }
 
 
 }
 
-function hitPoints() {
-
-}
-
-
-
-
-function getName() {
-  var sheet = document.getElementById("sheet");
-  var name = document.getElementById("characterName").value;
-  player.name = name;
-}
-
+//Funksjoner for å generere karakter
 function rollStats() {
   var strength = Math.floor(Math.random() * 70 + 30);
   var agility = Math.floor(Math.random() * 70 + 30);
@@ -57,4 +54,30 @@ function rollStats() {
   player.stats.smarts = smarts;
   player.stats.endurance = endurance;
   player.stats.charisma = charisma;
+}
+
+function hitPoints() {
+  if(player.gender === "male") {
+   if(player.age <= 18) {
+     player.hitPoints = player.stats.endurance + Math.floor(Math.random() * 100 + 20);
+   } else if(player.age <= 50) {
+     player.hitPoints = player.stats.endurance + Math.floor(Math.random() * 100 + 30);
+   } else if(player.age > 50) {
+     player.hitPoints = player.stats.endurance + Math.floor(Math.random() * 100 + 20);
+   }
+  } else if(player.gender === "female") {
+    if(player.age <= 18) {
+      player.hitPoints = player.stats.endurance + Math.floor(Math.random() * 100 + 15);
+    } else if(player.age <= 50) {
+      player.hitPoints = player.stats.endurance + Math.floor(Math.random() * 100 + 25);
+    } else if(player.age > 50) {
+    player.hitPoints = player.stats.endurance + Math.floor(Math.random() * 100 + 15);
+    }
+  }
+}
+
+function getName() {
+  var sheet = document.getElementById("sheet");
+  var name = document.getElementById("characterName").value;
+  player.name = name;
 }
